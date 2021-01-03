9863f916bcd9394b79bb73ddf720139e275122aa
Vladimir Eremeev <v.eremeev@samsung.com>
2021-01-03 19:01:37 +0300

    unpacked original archive

--- Source/FPSGame/Private/FPSCharacter.cpp
+++ Source/FPSGame/Private/FPSCharacter.cpp
@@ -10,92 +10,92 @@
 
 AFPSCharacter::AFPSCharacter()
 {
-    // Create a CameraComponent	
-    CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
-    CameraComponent->SetupAttachment(GetCapsuleComponent());
-    CameraComponent->SetRelativeLocation(FVector(0, 0, BaseEyeHeight)); // Position the camera
-    CameraComponent->bUsePawnControlRotation = true;
+	// Create a CameraComponent	
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	CameraComponent->SetupAttachment(GetCapsuleComponent());
+	CameraComponent->SetRelativeLocation(FVector(0, 0, BaseEyeHeight)); // Position the camera
+	CameraComponent->bUsePawnControlRotation = true;
 
-    // Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
-    Mesh1PComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh"));
-    Mesh1PComponent->SetupAttachment(CameraComponent);
-    Mesh1PComponent->CastShadow = false;
-    Mesh1PComponent->SetRelativeRotation(FRotator(2.0f, -15.0f, 5.0f));
-    Mesh1PComponent->SetRelativeLocation(FVector(0, 0, -160.0f));
+	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
+	Mesh1PComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh"));
+	Mesh1PComponent->SetupAttachment(CameraComponent);
+	Mesh1PComponent->CastShadow = false;
+	Mesh1PComponent->SetRelativeRotation(FRotator(2.0f, -15.0f, 5.0f));
+	Mesh1PComponent->SetRelativeLocation(FVector(0, 0, -160.0f));
 
-    // Create a gun mesh component
-    GunMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FP_Gun"));
-    GunMeshComponent->CastShadow = false;
-    GunMeshComponent->SetupAttachment(Mesh1PComponent, "GripPoint");
+	// Create a gun mesh component
+	GunMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FP_Gun"));
+	GunMeshComponent->CastShadow = false;
+	GunMeshComponent->SetupAttachment(Mesh1PComponent, "GripPoint");
 }
 
 
 void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-    // set up gameplay key bindings
-    check(PlayerInputComponent);
+	// set up gameplay key bindings
+	check(PlayerInputComponent);
 
-    PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
-    PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &AFPSCharacter::Fire);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &AFPSCharacter::Fire);
 
-    PlayerInputComponent->BindAxis("MoveForward", this, &AFPSCharacter::MoveForward);
-    PlayerInputComponent->BindAxis("MoveRight", this, &AFPSCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("MoveForward", this, &AFPSCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AFPSCharacter::MoveRight);
 
-    PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-    PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 }
 
 
 void AFPSCharacter::Fire()
 {
-    // try and fire a projectile
-    if (ProjectileClass)
-    {
-        FVector MuzzleLocation = GunMeshComponent->GetSocketLocation("Muzzle");
-        FRotator MuzzleRotation = GunMeshComponent->GetSocketRotation("Muzzle");
+	// try and fire a projectile
+	if (ProjectileClass)
+	{
+		FVector MuzzleLocation = GunMeshComponent->GetSocketLocation("Muzzle");
+		FRotator MuzzleRotation = GunMeshComponent->GetSocketRotation("Muzzle");
 
-        //Set Spawn Collision Handling Override
-        FActorSpawnParameters ActorSpawnParams;
-        ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
+		//Set Spawn Collision Handling Override
+		FActorSpawnParameters ActorSpawnParams;
+		ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
 
-        // spawn the projectile at the muzzle
-        GetWorld()->SpawnActor<AFPSProjectile>(ProjectileClass, MuzzleLocation, MuzzleRotation, ActorSpawnParams);
-    }
+		// spawn the projectile at the muzzle
+		GetWorld()->SpawnActor<AFPSProjectile>(ProjectileClass, MuzzleLocation, MuzzleRotation, ActorSpawnParams);
+	}
 
-    // try and play the sound if specified
-    if (FireSound)
-    {
-        UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetActorLocation());
-    }
+	// try and play the sound if specified
+	if (FireSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetActorLocation());
+	}
 
-    // try and play a firing animation if specified
-    if (FireAnimation)
-    {
-        // Get the animation object for the arms mesh
-        UAnimInstance* AnimInstance = Mesh1PComponent->GetAnimInstance();
-        if (AnimInstance)
-        {
-            AnimInstance->PlaySlotAnimationAsDynamicMontage(FireAnimation, "Arms", 0.0f);
-        }
-    }
+	// try and play a firing animation if specified
+	if (FireAnimation)
+	{
+		// Get the animation object for the arms mesh
+		UAnimInstance* AnimInstance = Mesh1PComponent->GetAnimInstance();
+		if (AnimInstance)
+		{
+			AnimInstance->PlaySlotAnimationAsDynamicMontage(FireAnimation, "Arms", 0.0f);
+		}
+	}
 }
 
 
 void AFPSCharacter::MoveForward(float Value)
 {
-    if (Value != 0.0f)
-    {
-        // add movement in that direction
-        AddMovementInput(GetActorForwardVector(), Value);
-    }
+	if (Value != 0.0f)
+	{
+		// add movement in that direction
+		AddMovementInput(GetActorForwardVector(), Value);
+	}
 }
 
 
 void AFPSCharacter::MoveRight(float Value)
 {
-    if (Value != 0.0f)
-    {
-        // add movement in that direction
-        AddMovementInput(GetActorRightVector(), Value);
-    }
+	if (Value != 0.0f)
+	{
+		// add movement in that direction
+		AddMovementInput(GetActorRightVector(), Value);
+	}
 }
--- Source/FPSGame/Public/FPSCharacter.h
+++ Source/FPSGame/Public/FPSCharacter.h
@@ -17,59 +17,56 @@ class UAnimSequence;
 UCLASS()
 class AFPSCharacter : public ACharacter
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
 protected:
 
-    /** Pawn mesh: 1st person view  */
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Mesh")
-    USkeletalMeshComponent* Mesh1PComponent;
+	/** Pawn mesh: 1st person view  */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Mesh")
+	USkeletalMeshComponent* Mesh1PComponent;
 
-    /** Gun mesh: 1st person view (seen only by self) */
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Mesh")
-    USkeletalMeshComponent* GunMeshComponent;
+	/** Gun mesh: 1st person view (seen only by self) */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Mesh")
+	USkeletalMeshComponent* GunMeshComponent;
 
-    /** First person camera */
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
-    UCameraComponent* CameraComponent;
+	/** First person camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	UCameraComponent* CameraComponent;
 
 public:
-    AFPSCharacter();
+	AFPSCharacter();
 
-    /** Projectile class to spawn */
-    UPROPERTY(EditDefaultsOnly, Category="Projectile")
-    TSubclassOf<AFPSProjectile> ProjectileClass;
+	/** Projectile class to spawn */
+	UPROPERTY(EditDefaultsOnly, Category="Projectile")
+	TSubclassOf<AFPSProjectile> ProjectileClass;
 
-    /** Sound to play each time we fire */
-    UPROPERTY(EditDefaultsOnly, Category="Gameplay")
-    USoundBase* FireSound;
+	/** Sound to play each time we fire */
+	UPROPERTY(EditDefaultsOnly, Category="Gameplay")
+	USoundBase* FireSound;
 
-    /** AnimMontage to play each time we fire */
-    UPROPERTY(EditDefaultsOnly, Category = "Gameplay")
-    UAnimSequence* FireAnimation;
-
-    UPROPERTY(BlueprintReadOnly, Category = "Gameplay")
-    bool bIsCarryingObjective;
+	/** AnimMontage to play each time we fire */
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay")
+	UAnimSequence* FireAnimation;
 
 protected:
-    
-    /** Fires a projectile. */
-    void Fire();
+	
+	/** Fires a projectile. */
+	void Fire();
 
-    /** Handles moving forward/backward */
-    void MoveForward(float Val);
+	/** Handles moving forward/backward */
+	void MoveForward(float Val);
 
-    /** Handles strafing movement, left and right */
-    void MoveRight(float Val);
+	/** Handles strafing movement, left and right */
+	void MoveRight(float Val);
 
-    virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
+	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
 
 public:
-    /** Returns Mesh1P subobject **/
-    USkeletalMeshComponent* GetMesh1P() const { return Mesh1PComponent; }
+	/** Returns Mesh1P subobject **/
+	USkeletalMeshComponent* GetMesh1P() const { return Mesh1PComponent; }
 
-    /** Returns FirstPersonCameraComponent subobject **/
-    UCameraComponent* GetFirstPersonCameraComponent() const { return CameraComponent; }
+	/** Returns FirstPersonCameraComponent subobject **/
+	UCameraComponent* GetFirstPersonCameraComponent() const { return CameraComponent; }
 
 };
 
