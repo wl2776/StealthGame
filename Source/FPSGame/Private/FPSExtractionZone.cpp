edb685fd294a37378759b87666cbaf39eacd8cde
Vladimir Eremeev <v.eremeev@samsung.com>
2021-01-05 21:14:23 +0300

    add disabling inputs on "Mission complete"

--- Source/FPSGame/Private/FPSExtractionZone.cpp
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -1,10 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "FPSExtractionZone.h"
 #include "Components/BoxComponent.h"
 #include "Components/DecalComponent.h"
 
+#include "FPSExtractionZone.h"
+#include "FPSCharacter.h"
+#include "FPSGameMode.h"
 
 // Sets default values
 AFPSExtractionZone::AFPSExtractionZone()
@@ -19,7 +21,6 @@ AFPSExtractionZone::AFPSExtractionZone()
     OverlapComp->SetHiddenInGame(false);
     OverlapComp->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::HandleOverlap);
 
-
     DecalComp = CreateDefaultSubobject<UDecalComponent>(TEXT("Decal"));
     DecalComp->DecalSize = { 200.0f, 200.0f, 200.0f };
     DecalComp->SetupAttachment(RootComponent);
@@ -29,4 +30,11 @@ AFPSExtractionZone::AFPSExtractionZone()
 void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
     UE_LOG(LogTemp, Log, TEXT("Overlapped!"));
+    AFPSCharacter* MyPawn = Cast<AFPSCharacter>(OtherActor);
+    if (MyPawn && MyPawn->bIsCarryingObjective) {
+        AFPSGameMode* GM = Cast<AFPSGameMode>(GetWorld()->GetAuthGameMode());
+        if (GM) {
+            GM->CompleteMission(MyPawn);
+        }
+    }
 }
